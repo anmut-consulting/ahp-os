04089840b7e27a96159852e868ba2820db42aaf8
Klaus <drklaus@bpmsg.org>
2024-06-28 15:12:33 +0800

    AhpSessionAdmin ru language file corrected

--- language/ru/AhpSessionAdminRU.php
+++ language/ru/AhpSessionAdminRU.php
@@ -3,74 +3,74 @@
 class AhpSessionAdminRU
 {
     public $titles = array(
-    'pageTitle'         => "AHP projects - AHP-OS",
-    'h1title'           => "<h1>AHP Project Administration</h1>",
-    'h2subTitle'        => "<h2>AHP-OS - Rational decision making made easy</h2>",
-    'h2ahpProjSummary'  => "<h2>Project Summary</h2>" ,
-    'h2ahpSessionMenu'  => "<h2>AHP Session Menu</h2>",
-    'h2ahpProjectMenu'  => "<h2>AHP Project Menu</h2>",
-    'h2myProjects'      => "<h2>My AHP Projects</h2>",
-    'h3groupInpLnk'     => "<h3>Group Input Link</h3>" ,
-    'h3projStruc'       => "<h3>Project Structure</h3>",
-    'h4hierDefTxt'      => "<h4>Hierarchy Definition Text</h4>",
+    'pageTitle'         => "Проекты AHP - AHP-OS",
+    'h1title'           => "<h1>Администрирование проекта AHP</h1>",
+    'h2subTitle'        => "<h2>AHP-OS - Простое принятие рациональных решений</h2>",
+    'h2ahpProjSummary'  => "<h2>Краткое описание проекта</h2>" ,
+    'h2ahpSessionMenu'  => "<h2>Меню AHP сессии</h2>",
+    'h2ahpProjectMenu'  => "<h2>Меню AHP проекта</h2>",
+    'h2myProjects'      => "<h2>Мои AHP проекты</h2>",
+    'h3groupInpLnk'     => "<h3>Ссылка для группового ввода данных</h3>" ,
+    'h3projStruc'       => "<h3>Структура проекта</h3>",
+    'h4hierDefTxt'      => "<h4>Текст определения иерархии</h4>",
 );
     public $msg = array(
-    'sDel'          => "<span class='msg'>Session <span class='var'>%s</span> was successfully deleted</span>",
-    'sDelp'         => "Participant(s) <span class='res'>%s</span> successfully deleted ",
-    'pwcCompl'      => "Pairwise comparisons under the name <span class='var'>%s</span> completed.",
-    'pClsd'         => "<p class='msg'>The project is closed. Click on <i>Toggle Proj. Status</i> to reopen.</p>",
-    'pStat1'        => "Project status changed to ",
-    'pStatO'        => "open.",
-    'pStatC'        => "closed.",
-    'selPart'       => "<span class='msg'>Selected participant(s): </span><span class='var'>%s</span>",
-    'hInfo1'        => "<span class='msg'>The decision hierarchy has defined priorities</span>",
-    'hInfo2'        => "<span class='msg'>. Project can be used to define alternatives. <br>Click on <i>Use Hierarchy</i></span>",
-    'hInfo3'        => "<span class='msg'> and project has <span class='var'>%g</span> defined alternatives.</span>",
-    'usrStat1'      => "<p class='msg'><small>AHP-OS has <span class='res'>%s</span> registered users, ",
-    'usrStat2'      => "<span class='res'>%g</span> active users in the last %g hours.</small></p>",
-    'usrStat3'      => "<p class='msg'>%s, you have <span class='res'>%g</span> projects. ",
-    'usrStat4'      => "Your program use index is <span class=res>%g%%</span>. ",
-    'usrDon1'       => "Please consider a <a href='ahp-news.php'>donation</a>",
-    'usrDon2'       => "Thanks for your donation"
+    'sDel'          => "<span class='msg'>Сессия <span class='var'>%s</span> успешно удалена</span>",
+    'sDelp'         => "Участник(и) <span class='res'>%s</span> успешно удалены ",
+    'pwcCompl'      => "Попарные сравнения под названием <span class='var'>%s</span> завершены.",
+    'pClsd'         => "<p class='msg'>Проект закрыт. Нажмите на <i>Переключение статуса проекта</i> для повторного открытия.</p>",
+    'pStat1'        => "Статус проекта изменен на",
+    'pStatO'        => "открыт.",
+    'pStatC'        => "закрыт.",
+    'selPart'       => "<span class='msg'>Выбранный участник(и): </span><span class='var'>%s</span>",
+    'hInfo1'        => "<span class='msg'>В иерархии принятия решений определены приоритеты</span>",
+    'hInfo2'        => "<span class='msg'>. Проект может быть использован для определения альтернатив. <br>Нажмите на <i>Использовать Иерархию</i></span>",
+    'hInfo3'        => "<span class='msg'> и у проекта <span class='var'>%g</span> определены альтернативы.</span>",
+    'usrStat1'      => "<p class='msg'><small>В AHP-OS <span class='res'>%s</span> зарегистрированных пользователей, ",
+    'usrStat2'      => "<span class='res'>%g</span> активных пользователей за последние %g часа(ов).</small></p>",
+    'usrStat3'      => "<p class='msg'>%s, у вас есть <span class='res'>%g</span> проекта(ов). ",
+    'usrStat4'      => "Индекс использования вашей программы равен <span class=res>%g%%</span>. ",
+    'usrDon1'       => "Пожалуйста, рассмотрите возможность <a href='ahp-news.php'>пожертвования</a>",
+    'usrDon2'       => "Спасибо за ваше пожертвование"
 );
     public $err = array(
-    'invSess1'      => "Invalid Session Code.",
-    'invSess2'      => "Invalid Session Code in url.",
-    'noAuth'        => "As you are not the project author, you are not allowed to delete participants.",
-    'pClosed'       => "Project is closed. No pairwise comparison input allowed.",
-    'noDel'         => "could not be deleted.",
-    'sLmt'          => "<p><span class='err'>Session limit reached.</span> Please delete some old sessions first. </p>"
+    'invSess1'      => "едопустимый код сессии.",
+    'invSess2'      => "Неверный код сессии в URL-адресе.",
+    'noAuth'        => "Поскольку вы не являетесь автором проекта, вы не можете удалять участников.",
+    'pClosed'       => "Проект закрыт. Ввод данных для попарного сравнения прекращен.",
+    'noDel'         => "не удалось удалить.",
+    'sLmt'          => "<p><span class='err'>Достигнут лимит сессий.</span> Пожалуйста, сначала удалите несколько старых сессий. </p>"
 );
     public $info = array(
-    'sc'            => "The session code is <span class='var'>%s</span>.",
-    'scLnk1'        => "Provide this session code or the following link to your participants: </span><br>",
+    'sc'            => "Код сессии <span class='var'>%s</span>.",
+    'scLnk1'        => "Направьте этот код сессии или следующую ссылку вашим участникам: </span><br>",
     'scLnk2'        => "<textarea rows='1' cols='78'>%s?sc=%s</textarea><br>",
-    'scLnk3'        => "Go to link above: <a href='%s?sc=%s' >Group Input</a><br>",
-    'pOpen1'        => "Click on the session link in the table below to open a project. ",
-    'pOpen2'        => "<br>Create a <a href='%s'>new hierarchy</a>.",
+    'scLnk3'        => "Перейдите по ссылке выше: <a href='%s?sc=%s' >Групповой ввод</a><br>",
+    'pOpen1'        => "Нажмите на ссылку сессии в таблице ниже, чтобы открыть проект. ",
+    'pOpen2'        => "<br>Создать <a href='%s'>новую иерархию</a>.",
     'logout'        => "<div class='entry-content'>
-                       On the AHP project administration page you can manage your AHP projects: 
-                       create new hierarchies, open, edit or delete and view existing projects. 
-                       <p class='msg'>You need to be a registered user and login to handle projects.</p>
-                       <p><a href='%s'>back</a></p></div>"
+                       На странице администрирования проекта AHP вы можете управлять своими проектами: 
+                       создавать новые иерархии, открывать, редактировать или удалять, а также просматривать существующие проекты.. 
+                       <p class='msg'>Вам необходимо быть зарегистрированным пользователем и войти в систему, чтобы управлять проектами.</p>
+                       <p><a href='%s'>назад</a></p></div>"
 );
     public $mnu = array(
-    'lgd1'          => "Session Administration Menu",
-    'lbl1'          => "Project Session Code: ",
-    'btnps1'        => "Open Project",
-    'btnps2'        => "New Project",
-    'btnps3'        => "Done",
-    'btnps4'        => "Import Project",
-    'lgd2'          => "Project Administration Menu",
-    'btnpa1'        => "View Result",
-    'btnpa2'        => "PWC Input",
-    'btnpa3'        => "Use Hierarchy",
-    'btnpa4'        => "Rename",
-    'btnpa5'        => "Edit",
-    'btnpa6'        => "Del Sel. Part.(s)",
-    'btnpa7'        => "Delete Project",
-    'btnpa8'        => "Toggle Project Status",
-    'btnpa9'        => "Done",
-    'btnpa10'       => "Export Project"
+    'lgd1'          => "Меню администрирования сессии",
+    'lbl1'          => "Код сессии проекта: ",
+    'btnps1'        => "Открыть проект",
+    'btnps2'        => "Новый проект",
+    'btnps3'        => "Готово",
+    'btnps4'        => "Импорт проекта",
+    'lgd2'          => "Меню администрирования проекта",
+    'btnpa1'        => "Просмотр результата",
+    'btnpa2'        => "Запуск попарного сравнения",
+    'btnpa3'        => "Используйте иерархию",
+    'btnpa4'        => "Переименовать",
+    'btnpa5'        => "Редактировать",
+    'btnpa6'        => "Удалить выбр.часть(и)",
+    'btnpa7'        => "Удалить проект",
+    'btnpa8'        => "Переключить статус проекта",
+    'btnpa9'        => "Готово",
+    'btnpa10'       => "Экспорт проекта"
 );
 }
